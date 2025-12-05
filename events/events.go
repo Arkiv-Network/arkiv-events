b307ad88d4a36945ef38a27f36f5d0828df37570
Dragan Milic <dragan@netice9.com>
2025-12-05 13:36:48 +0100

    added expire event, added proper json tags

--- events/events.go
+++ events/events.go
@@ -4,16 +4,18 @@ import (
 	"github.com/ethereum/go-ethereum/common"
 )
 
+type OPExpire common.Hash
+
 type OPDelete common.Hash
 
 type OPCreate struct {
-	Key               common.Hash
-	ContentType       string
-	BTL               uint64
-	Owner             common.Address
-	Content           []byte
-	StringAttributes  map[string]string
-	NumericAttributes map[string]uint64
+	Key               common.Hash       `json:"key"`
+	ContentType       string            `json:"content_type"`
+	BTL               uint64            `json:"btl"`
+	Owner             common.Address    `json:"owner"`
+	Content           []byte            `json:"content"`
+	StringAttributes  map[string]string `json:"string_attributes"`
+	NumericAttributes map[string]uint64 `json:"numeric_attributes"`
 }
 
 type OPContext struct {
@@ -23,40 +25,41 @@ type OPContext struct {
 }
 
 type OPUpdate struct {
-	Key               common.Hash
-	ContentType       string
-	BTL               uint64
-	Owner             common.Address
-	Content           []byte
-	StringAttributes  map[string]string
-	NumericAttributes map[string]uint64
+	Key               common.Hash       `json:"key"`
+	ContentType       string            `json:"content_type"`
+	BTL               uint64            `json:"btl"`
+	Owner             common.Address    `json:"owner"`
+	Content           []byte            `json:"content"`
+	StringAttributes  map[string]string `json:"string_attributes"`
+	NumericAttributes map[string]uint64 `json:"numeric_attributes"`
 }
 
 type OPExtendBTL struct {
-	Key common.Hash
-	BTL uint64
+	Key common.Hash `json:"key"`
+	BTL uint64      `json:"btl"`
 }
 
 type OPChangeOwner struct {
-	Key   common.Hash
-	Owner common.Address
+	Key   common.Hash    `json:"key"`
+	Owner common.Address `json:"owner"`
 }
 
 type Operation struct {
-	TxIndex     uint64
-	OpIndex     uint64
-	Delete      *OPDelete
-	Create      *OPCreate
-	Update      *OPUpdate
-	ExtendBTL   *OPExtendBTL
-	ChangeOwner *OPChangeOwner
+	TxIndex     uint64         `json:"tx_index"`
+	OpIndex     uint64         `json:"op_index"`
+	Delete      *OPDelete      `json:"delete,omitempty"`
+	Expire      *OPExpire      `json:"expire,omitempty"`
+	Create      *OPCreate      `json:"create,omitempty"`
+	Update      *OPUpdate      `json:"update,omitempty"`
+	ExtendBTL   *OPExtendBTL   `json:"extend_btl,omitempty"`
+	ChangeOwner *OPChangeOwner `json:"change_owner,omitempty"`
 }
 
 type Block struct {
-	Number     uint64
-	Operations []Operation
+	Number     uint64      `json:"number"`
+	Operations []Operation `json:"operations"`
 }
 
 type BlockBatch struct {
-	Blocks []Block
+	Blocks []Block `json:"blocks"`
 }
